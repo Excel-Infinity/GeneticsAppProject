b26a0ee2a862ca43a3f9f31f3964b3cb38be0dd9
winge314 <96355821+winge314@users.noreply.github.com>
2023-02-17 11:03:07 -0800

    don't delete anything important please

---
+++ bioloagya thing/NatSel.js
@@ -0,0 +1,8 @@
+
+run();
+
+// MODIFY
+/*
+Parameters: individuals (integer), p (float 0-1), generations (integer), natsel rates (3 floats 0-1)
+Outputs: Array of gens; each gen is an array of 3 integers [# ind rec, het, dom]
+*/
