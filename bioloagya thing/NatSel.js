2cb21e5109bb7a8e0080815997416edfd195e791
winge314 <96355821+winge314@users.noreply.github.com>
2023-02-17 11:54:30 -0800

    natsel is actually readable now (incredible)

--- bioloagya thing/NatSel.js
+++ bioloagya thing/NatSel.js
@@ -2,64 +2,60 @@ function random(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
-function run(numIndividuals, pFloat, numGenerations, naturalSelection) {
+function calcGens(numIndividuals, pFloat, numGenerations, naturalSelection) {
     var ind = numIndividuals;
     var p = pFloat;
     var q = 1 - p;
-    var d = p * ind;
-    var r = q * ind;
+    var totalDomAllele = p * ind;
+    var totalRecAllele = q * ind;
     var gens = [[0, 0, 0]];
     var numGens = numGenerations;
-    var natSel = [naturalSelection[0] * 100, naturalSelection[1] * 100, naturalSelection[2] * 100];
-    var why = true;
+    var natSelRates = [naturalSelection[0] * 100, naturalSelection[1] * 100, naturalSelection[2] * 100];
+    var isFirstGen = true;
     for (var i = 1; i < numGens; i++) {
-        var dNew = d;
-        var rNew = r;
+        var tempDom = totalDomAllele;
+        var tempRec = totalRecAllele;
         var dom = 0, hetero = 0, rec = 0;
         for (var j = 0; j < ind; j++) {
-            var g1 = random(0, dNew + rNew - 1) < dNew ? 1 : 0;
-            var g2 = random(0, dNew + rNew - 1) < dNew ? 1 : 0;
-            var gSum = g1 + g2;
-            if (gSum == 0) {
-                if (random(0, 100) > natSel[2]) {
+            var allele1 = random(0, tempDom + tempRec - 1) < tempDom ? 1 : 0;
+            var allele2 = random(0, tempDom + tempRec - 1) < tempDom ? 1 : 0;
+            var alleleSum = allele1 + allele2;
+            if (alleleSum == 0) {
+                if (random(0, 100) > natSelRates[2]) {
                     rec++;
                 }
-                if (why) {
+                if (isFirstGen) {
                     gens[0][0]++;
                 }
-            } else if (gSum == 1) {
-                if (random(0, 100) > natSel[1]) {
+            } else if (alleleSum == 1) {
+                if (random(0, 100) > natSelRates[1]) {
                     hetero++;
                 }
-                if (why) {
+                if (isFirstGen) {
                     gens[0][1]++;
                 }
-            } else if (gSum == 2) {
-                if (random(0, 100) > natSel[0]) {
+            } else if (alleleSum == 2) {
+                if (random(0, 100) > natSelRates[0]) {
                     dom++;
                 }
-                if (why) {
+                if (isFirstGen) {
                     gens[0][2]++;
                 }
             }
-            dNew -= gSum;
-            rNew -= 2 - gSum;
+            tempDom -= alleleSum;
+            tempRec -= 2 - alleleSum;
         }
-        why = false;
-        d = 2 * dom + hetero;
-        r = 2 * rec + hetero;
-        // normalize d and r to ind
-        d = Math.round(ind * d / (d + r));
-        r = ind - d;
-        // normalize rec, hetero, dom to ind
+        isFirstGen = false;
+        totalDomAllele = 2 * dom + hetero;
+        totalRecAllele = 2 * rec + hetero;
+        // normalize to ind
+        totalDomAllele = Math.round(ind * totalDomAllele / (totalDomAllele + totalRecAllele));
+        totalRecAllele = ind - totalDomAllele;
         var total = rec + hetero + dom;
         rec = Math.round(ind * rec / total);
         hetero = Math.round(ind * hetero / total);
         dom = ind - rec - hetero;
         gens[i] = [rec, hetero, dom];
-    }
-    for (var i = 0; i < gens.length; i++) {
-        console.log(i + ":\t" + gens[i][0] + "\t" + gens[i][1] + "\t" + gens[i][2]);
     }
     return gens;
 }
@@ -69,7 +65,7 @@ Parameters: (
     individuals (integer),
     p (float 0-1), <- maps to dominant allele
     generations (integer),
-    natsel rates (3 floats 0-1)
+    natSelRates rates (3 floats 0-1)
 )
 Outputs: Array of gens; each gen is an array of 3 integers [# ind rec, # ind hetero, # ind dom]
 */
