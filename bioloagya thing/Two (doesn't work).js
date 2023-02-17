30cdcfb340533ef411684bfbea163624820e9364
winge314 <96355821+winge314@users.noreply.github.com>
2023-02-17 10:59:53 -0800

    fixed two

--- bioloagya thing/Two (doesn't work).js
+++ bioloagya thing/Two (doesn't work).js
@@ -24,11 +24,11 @@ function run() {
             var g1 = random(0, dNew + rNew - 1) < dNew ? 1 : 0;
             var g2 = random(0, dNew + rNew - 1) < dNew ? 1 : 0;
             var gSum = g1 + g2;
-            if (gSum == 0 && random(0, 100) <= natSel[2]) {
+            if (gSum == 0) {
                 hr++;
-            } else if (gSum == 1 && random(0, 100) <= natSel[1]) {
+            } else if (gSum == 1) {
                 he++;
-            } else if (gSum == 2 && random(0, 100) <= natSel[0]) {
+            } else {
                 hd++;
             }
             dNew -= gSum;
@@ -36,6 +36,10 @@ function run() {
         }
         gens[i] = [[hd, he, hr], d / (d + r), r / (d + r)];
         console.log(i + ":\t" + gens[i][0][0] + "\t" + gens[i][0][1] + "\t" + gens[i][0][2] + "\t" + gens[i][1] + "\t" + gens[i][2]);
+        // kill some off in a specific ratio, determined by natSel
+        var hd = Math.round(gens[i][0][0] * (1 - natSel[0] / 100));
+        var he = Math.round(gens[i][0][1] * (1 - natSel[1] / 100));
+        var hr = Math.round(gens[i][0][2] * (1 - natSel[2] / 100));
         d = 2 * hd + he;
         r = 2 * hr + he;
         // normalize d and r to ind
