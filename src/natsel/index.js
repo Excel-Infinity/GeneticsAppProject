da5885b34cec3738ad46bb40ec8cf873b4801d35
winge314 <96355821+winge314@users.noreply.github.com>
2023-03-14 11:53:45 -0700

    natsel fixes

--- src/natsel/index.js
+++ src/natsel/index.js
@@ -35,6 +35,7 @@ const aa_chance_input = /** @type {HTMLInputElement} */  (document.getElementByI
 const Aa_chance_input = /** @type {HTMLInputElement} */  (document.getElementById("he-chance"));
 const AA_chance_input = /** @type {HTMLInputElement} */  (document.getElementById("hd-chance"));
 const num_gens_input  = /** @type {HTMLInputElement} */  (document.getElementById("num-gens"));
+const seed_input      = /** @type {HTMLInputElement} */  (document.getElementById("seed"));
 
 const start_canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("start-graph"));
 const end_canvas   = /** @type {HTMLCanvasElement} */ (document.getElementById("end-graph"));
@@ -66,8 +67,9 @@ run_button.addEventListener("click", () => {
 	const AA_chance = parseFloat(AA_chance_input.value);
 	const num_gens = parseFloat(num_gens_input.value);
 	const chances = [aa_chance, Aa_chance, AA_chance];
+	const seed = parseInt(seed_input.value);
 
-	const gens = runSelection(ind, p, num_gens, chances);
+	const gens = runSelection(ind, p, num_gens, chances, seed);
 	const last_gen = gens[gens.length - 1];
 	start_chart.config.data.datasets[0].data = gens[0];
 	end_chart.config.data.datasets[0].data = last_gen;
--- src/natsel/natsel.js
+++ src/natsel/natsel.js
@@ -1,31 +1,48 @@
 /**
+ * @param {number} a seed for random number generator
+ * @returns {() => number} random number between 0 and 1
+ * @description https://github.com/bryc/code/blob/master/jshash/PRNGs.md
+ * I needed a seedable random number generator
+ */
+function mulberry32(a) {
+    return function() {
+      a |= 0; a = a + 0x6D2B79F5 | 0;
+      var t = Math.imul(a ^ a >>> 15, 1 | a);
+      t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
+/**
  * @param {number} a lower bound
  * @param {number} b upper bound
+ * @param {() => number} rand random number generator
  * @returns {number} random integer between a and b inclusive
  */
-function random(a, b) {
-    return Math.floor(Math.random() * (b - a + 1)) + a;
+function random(a, b, rand) {
+    return Math.floor(rand() * (b - a + 1)) + a;
 }
 
 /**
  * @param {number[]} pop gen information of the form [num aa, num Aa, num AA]
  * @param {number[]} natsel array form of survival chances (0-1) [aa chance, Aa chance, AA chance]
+ * @param {() => number} rand random number generator
  * @returns {number[]} new gen information of the form [num aa, num Aa, num AA]
  */
-function natSel(pop, natsel) {
+function natSel(pop, natsel, rand) {
     const newPopulation = [0, 0, 0];
     for (let i = 0; i < pop[0]; i++) {
-        if (Math.random() < natsel[0]) {
+        if (rand() < natsel[0]) {
             newPopulation[0]++;
         }
     }
     for (let i = 0; i < pop[1]; i++) {
-        if (Math.random() < natsel[1]) {
+        if (rand() < natsel[1]) {
             newPopulation[1]++;
         }
     }
     for (let i = 0; i < pop[2]; i++) {
-        if (Math.random() < natsel[2]) {
+        if (rand() < natsel[2]) {
             newPopulation[2]++;
         }
     }
@@ -35,41 +52,36 @@ function natSel(pop, natsel) {
 /**
  * @param {number[]} population gen information of the form [num aa, num Aa, num AA]
  * @param {number} totalIndividuals total number of individuals in the population
- * @returns {number} allele sum; 0 for aa, 1 for Aa, 2 for AA
+ * @param {() => number} rand random number generator
+ * @returns {number[]} new gen information of the form [num aa, num Aa, num AA]
  */
-function reproduceIndividual(population, totalIndividuals) {
-    let alleleSum = 0;
-    const parent1 = random(0, totalIndividuals - 1);
-    const parent2 = random(0, totalIndividuals - 1);
-    if (parent1 < population[0]) { // aa
-        alleleSum += 0;
-    } else if (parent1 < population[0] + population[1]) { // Aa
-        alleleSum += random(0, 1);
-    } else { // AA
-        alleleSum += 1;
-    }
-    if (parent2 < population[0]) { // aa
-        alleleSum += 0;
-    } else if (parent2 < population[0] + population[1]) { // Aa
-        alleleSum += random(0, 1);
-    } else { // AA
-        alleleSum += 1;
-    }
-    return alleleSum;
-}
-
-/**
- * @param {number[]} population gen information of the form [num aa, num Aa, num AA]
- * @param {number} totalIndividuals total number of individuals in the population
- * @returns {number[]} new population of the form [num aa, num Aa, num AA]
- */
-function reproduce(population, totalIndividuals) {
-    const newPopulation = [0, 0, 0];
+function reproduce(population, totalIndividuals, rand) {
+    const aa = population[0];
+    const Aa = population[1];
+    const AA = population[2];
+    const totalRecessive = 2 * aa + Aa;
+    const totalDominant = 2 * AA + Aa;
+    // hardy weinberg
+    const p = totalDominant / (2 * totalIndividuals);
+    const q = 1 - p;
+    const p2 = p * p;
+    const q2 = q * q;
+    const pq = 2 * p * q;
+    const p2Ind = Math.round(totalIndividuals * p2);
+    const q2Ind = Math.round(totalIndividuals * q2);
+    const pqInd = Math.round(totalIndividuals * pq);
+    const gen = [0, 0, 0];
     for (let i = 0; i < totalIndividuals; i++) {
-        const alleleSum = reproduceIndividual(population, totalIndividuals);
-        newPopulation[alleleSum]++;
+        const individual = random(0, totalIndividuals - 1, rand);
+        if (individual < p2Ind) {
+            gen[2]++;
+        } else if (individual < p2Ind + pqInd) {
+            gen[1]++;
+        } else {
+            gen[0]++;
+        }
     }
-    return newPopulation;
+    return gen;
 }
 
 /**
@@ -89,12 +101,13 @@ function normalize(population, totalIndividuals) {
 /**
  * @param {number[]} prevGen previous gen information of the form [num aa, num Aa, num AA]
  * @param {number[]} naturalSelection array form of survival chances (0-1) [aa chance, Aa chance, AA chance]
+ * @param {() => number} rand random number generator
  * @returns {number[]} gen information of the form [num aa, num Aa, num AA]
  */
-function gen(prevGen, naturalSelection) {
+function gen(prevGen, naturalSelection, rand) {
     const totalIndividuals = prevGen[0] + prevGen[1] + prevGen[2];
-    let newGen = natSel(prevGen, naturalSelection);
-    newGen = reproduce(newGen, totalIndividuals);
+    let newGen = natSel(prevGen, naturalSelection, rand);
+    //newGen = reproduce(newGen, totalIndividuals, rand);
     newGen = normalize(newGen, totalIndividuals);
     return newGen;
 }
@@ -104,15 +117,17 @@ function gen(prevGen, naturalSelection) {
  * @param {number} pFloat fraction of alleles being dominant
  * @param {number} numGenerations number of generations to run
  * @param {number[]} naturalSelection array form of survival chances (0-1) [aa chance, Aa chance, AA chance]
+ * @param {number} seed seed for random number generator
  * @returns {number[][]} array of gen information, each element is of the form [num aa, num Aa, num AA]
  */
-function run(numIndividuals, pFloat, numGenerations, naturalSelection) {
+function run(numIndividuals, pFloat, numGenerations, naturalSelection, seed) {
     const p = pFloat;
     const q = 1 - p;
     const gens = [[Math.round(numIndividuals * q * q), Math.round(numIndividuals * 2 * q * p), Math.round(numIndividuals * p * p)]];
+    const rand = mulberry32(seed);
     for (let i = 1; i < numGenerations; i++) {
         const prevGen = gens[i - 1];
-        gens[i] = gen(prevGen, naturalSelection);
+        gens[i] = gen(prevGen, naturalSelection, rand);
     }
     return gens;
 }
