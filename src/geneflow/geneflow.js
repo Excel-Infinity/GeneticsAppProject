b2cab92e4f9db7152619a35a896daa1f6fc3a1b1
winge314 <96355821+winge314@users.noreply.github.com>
2023-03-21 09:07:07 -0700

    this should not have been pushed

--- src/geneflow/geneflow.js
+++ src/geneflow/geneflow.js
@@ -23,10 +23,10 @@ function flowOut(population, flowRate, rand) {
     const gen = [population[0], population[1], population[2]];
     const total = population[0] + population[1] + population[2];
     for (let i = 0; i < -flowRate; i++) {
-        const randNum = rand();
-        if (randNum < population[0] / total) {
+        const randNum = rand() * total;
+        if (randNum < population[0]) {
             gen[0]--;
-        } else if (randNum < (population[0] + population[1]) / total) {
+        } else if (randNum < (population[0] + population[1])) {
             gen[1]--;
         } else {
             gen[2]--;
@@ -46,10 +46,10 @@ function flowIn(population, flowRate, otherPop, rand) {
     const gen = [population[0], population[1], population[2]];
     const total = otherPop[0] + otherPop[1] + otherPop[2];
     for (let i = 0; i < flowRate; i++) {
-        const randNum = rand();
-        if (randNum < otherPop[0] / total) {
+        const randNum = rand() * total;
+        if (randNum < otherPop[0]) {
             gen[0]++;
-        } else if (randNum < (otherPop[0] + otherPop[1]) / total) {
+        } else if (randNum < (otherPop[0] + otherPop[1])) {
             gen[1]++;
         } else {
             gen[2]++;
@@ -59,15 +59,33 @@ function flowIn(population, flowRate, otherPop, rand) {
 }
 
 /**
+ * @param {number} p fraction of alleles being dominant
+ * @param {number} q fraction of alleles being recessive
+ * @returns {number} index of the genotype to add to the new population
+ */
+function genInd(p, q) {
+    const randNum = Math.random();
+    if (randNum < q * q) {
+        return 0;
+    } else if (randNum < q * q + 2 * q * p) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+/**
  * @param {number[]} population gen information of the form [num aa, num Aa, num AA]
  * @param {number} totalIndividuals total number of individuals in the population
  * @returns {number[]} normalized population of the form [num aa, num Aa, num AA]
  */
-function normalize(population, totalIndividuals) {
+function reproduce(population, totalIndividuals) {
     const newPopulation = [0, 0, 0];
-    const shortTotalIndividuals = population[0] + population[1] + population[2];
-    for (let i = 0; i < population.length; i++) {
-        newPopulation[i] = Math.round(totalIndividuals * population[i] / shortTotalIndividuals);
+    const shortTotal = population[0] + population[1] + population[2];
+    const p = ((population[1] / 2) + population[2]) / shortTotal;
+    const q = 1 - p;
+    for (let i = 0; i < totalIndividuals; i++) {
+        newPopulation[genInd(p, q)]++;
     }
     return newPopulation;
 }
@@ -82,7 +100,7 @@ function normalize(population, totalIndividuals) {
  */
 function gen(prevGen, totalIndividuals, flowRate, otherPop = prevGen, rand) {
     let newGen = flow(prevGen, flowRate, otherPop, rand);
-    newGen = normalize(newGen, totalIndividuals);
+    newGen = reproduce(newGen, totalIndividuals);
     return newGen;
 }
 
